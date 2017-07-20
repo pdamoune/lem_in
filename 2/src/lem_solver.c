25ae81b0321fdc28ac91d17dab48cbabf469e0d2
philippe damoune <philippedamoune@gmail.com>
2017-07-20 15:28:52 +0200

    update

--- 2/src/lem_solver.c
+++ 2/src/lem_solver.c
@@ -6,42 +6,47 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/13 03:17:22 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/07/13 16:34:03 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/07/15 19:31:35 by philippedamoune  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/lem_in.h"
 
-t_list	*lem_find_path(t_list *paths, t_list *path, t_room *room)
+int		lem_find_path(t_list *paths, t_list *path, t_room *room)
 {
 	t_list	*links;
 	t_room	*next_room;
+	static int i = 0;
+	static int shrt_len = INT_MAX;
+	// int		len;
 
 	links = room->links;
 	while (links)
 	{
 		next_room = links->content;
+		i++;
 		if (!next_room->busy)
 		{
 			next_room->busy = 1;
 			ft_lstadd_last(&path, ft_lstptr(next_room));
 			if (next_room->position == END - 1)
 			{
-				if (!g_paths)
-					 g_paths = ft_lstptr((ft_lstdup(path)));
-				else
-					ft_lstadd_last(&g_paths, ft_lstptr(ft_lstdup(path)));
-				next_room->busy = 0;
 				ft_lstclr_last(&path);
-				return (path);
+				shrt_len = ft_lstlen(path);
+				ft_lstadd_last(&g_paths, ft_lstptr(ft_lstdup(path)));
+
+				next_room->busy = 0;
+				i--;
+				return (shrt_len);
 			}
-			lem_find_path(paths, path, next_room);
+			shrt_len = lem_find_path(paths, path, next_room);
 			next_room->busy = 0;
 			ft_lstclr_last(&path);
 		}
 		links = links->next;
+		i--;
 	}
-	return (NULL);
+	return (shrt_len);
 }
 
 // static void 	del(void *room, size_t size)
@@ -60,7 +65,7 @@ int		lem_get_paths(t_list *paths, t_room *room)
 
 
 	start = room;
-	path = ft_lstptr(room);
+	path = NULL;
 	links = room->links;
 	while (links)
 	{
@@ -68,18 +73,19 @@ int		lem_get_paths(t_list *paths, t_room *room)
 		room->busy = 1;
 		ft_lstadd_last(&path, ft_lstptr(room));
 		lem_find_path(paths, path, room);
-		room->busy = 0;
 		ft_lstptrdel(&path);
-		if ((links = links->next))
-			path = ft_lstptr(start);
+		links = links->next;
+		// 	path = ft_lstptr(start);
 	}
 	return (0);
 }
 
-void 	ft_lstswap(t_list *lst1, t_list *lst2)
+void 	ft_lstswap_content(t_list *lst1, t_list *lst2)
 {
 	t_list	*tmp;
 
+	if (!lst1 || !lst2)
+		return ;
 	tmp = lst1->content;
 	lst1->content = lst2->content;
 	lst2->content = tmp;
@@ -98,7 +104,7 @@ int		ft_lstsortlen(t_list *list)
 		len = ft_lstlen(tmp->content);
 		if (tmp->next && ft_lstlen(tmp->next->content) < (size_t)len)
 		{
-			ft_lstswap(tmp, tmp->next);
+			ft_lstswap_content(tmp, tmp->next);
 			tmp = list;
 		}
 		else
@@ -107,32 +113,17 @@ int		ft_lstsortlen(t_list *list)
 	return (len);
 }
 
-// void 	ft_on_essaye(t_list *all_paths1, t_list *all_paths2)
-// {
-// 	t_list	*path1;
-// 	t_list	*path2;
-//
-// 	all_paths2 = all_paths1;
-// 	while (all_paths1)
-// 	{
-// 	}
-//
-//
-// }
+
 
 int		lem_solver(void)
 {
 	t_room	*start;
-	// t_list	*path;
 	t_list	*paths;
-	t_list	*tmp;
-	size_t	lst_len;
-	size_t	len;
-	char	**graph;
-	// long	*graph;
+
+	// char	*graph;
+	int		len_paths;
 	int		i;
 	int		j;
-
 
 	ft_printf("\n========   SOLVER   ========\n");
 	start = lem_get_start(g_rooms);
@@ -142,37 +133,8 @@ int		lem_solver(void)
 	lem_get_paths(g_paths, start);
 	paths = g_paths;
 	ft_lstsortlen(paths);
-	lst_len = ft_lstlen(paths);
-	len = ft_lstlen(g_rooms);
-	ft_putnbrel(lst_len);
-	ft_putnbrel(len);
-	graph = ft_memalloc(sizeof(char *) * (lst_len + 1));
-	// ft_on_essaye(g_paths, paths);
-	while (paths)
-	{
-		graph[i] = ft_memalloc(len + 1);
-		tmp = paths->content;
-		while (tmp)
-		{
-			if (((t_room *)tmp->content)->position < 1)
-				graph[i][((t_room *)tmp->content)->room_number] = 1;
-			tmp = tmp->next;
-		}
-		paths = paths->next;
-		i++;
-	}
-	// i = 0;
-	// j = len;
-	// ft_printf("coucou\n");
-	//
-	// while (graph[i])
-	// {
-	// 	while (j-- > 0)
-	// 		ft_printf("%d", graph[i][j]);
-	// 	ft_printf("\n");
-	// 	j = len;
-	// 	i++;
-	// }
+	len_paths = ft_lstlen(paths);
+	// lem_get_not_crossed
 
 
 	ft_printf("\n======== FIN SOLVER ========\n");
--- 2/src/main.c
+++ 2/src/main.c
@@ -6,7 +6,7 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/10 11:25:38 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/07/13 06:49:38 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/07/14 16:49:34 by philippedamoune  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		main(void)
 	lem_set_globales();
 	lem_parser();
 	lem_solver();
-	lem_display(2, "rooms", "paths");
+	lem_display(1, "paths");
 	lem_free(0);
 	return (0);
 }
